54869df277b906d108fd9a006930c2afd0f41e84
karmacoma92 <reeopp@hotmail.com>
2019-12-01 19:55:59 +0100

    Date change

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,6 +1,6 @@
 #define APP_NAME                "ESPURNA"
 #define APP_VERSION             "1.14.0.PA4-dev"
-#define APP_REVISION            "191129"
+#define APP_REVISION            "191130"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             3

477709f85bbfbaa52085702a24e8fdfd3a9a6ce8
karmacoma92 <reeopp@hotmail.com>
2019-12-11 21:29:28 +0100

    fix version.h

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,10 +1,6 @@
 #define APP_NAME                "ESPURNA"
-<<<<<<< HEAD
 #define APP_VERSION             "1.14.1.PA6-dev"
 #define APP_REVISION            "191211"
-=======
-#define APP_VERSION             "1.14.1-dev"
->>>>>>> upstream/dev
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             3

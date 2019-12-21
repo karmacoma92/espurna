65ae21760346d9668f14541c2062fcaaaaa61f6b
karmacoma92 <reeopp@hotmail.com>
2019-12-21 10:24:46 +0100

    version

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,6 +1,6 @@
 #define APP_NAME                "ESPURNA"
-#define APP_VERSION             "1.14.1.PA8-dev"
-#define APP_REVISION            "191218"
+#define APP_VERSION             "1.14.1.PA9-dev"
+#define APP_REVISION            "191221"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             3

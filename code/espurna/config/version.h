bee26ee8b16f12adaf6163ac6adc57f9e5af8619
reeopp@hotmail.com <reeopp@hotmail.com>
2020-03-03 18:38:47 +0100

    200303

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -6,7 +6,7 @@
 
 #define APP_NAME                "ESPURNA"
 #define APP_VERSION             "1.14.2.PA5-dev"
-#define APP_REVISION            "200219"
+#define APP_REVISION            "200303"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             3

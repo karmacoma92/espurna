a7099c7778bbdf9c855399c23fae558a86bc2d03
reeopp@hotmail.com <reeopp@hotmail.com>
2020-03-14 20:37:34 +0100

    1.14.2.PA8-dev - 200314 Merge remote-tracking branch 'upstream/dev' into adpted-2

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -5,8 +5,8 @@
 #pragma once
 
 #define APP_NAME                "ESPURNA"
-#define APP_VERSION             "1.14.2.PA7-dev"
-#define APP_REVISION            "200313"
+#define APP_VERSION             "1.14.2.PA8-dev"
+#define APP_REVISION            "200314"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             4
